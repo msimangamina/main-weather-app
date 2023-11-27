3d82d733885b84a2c1d76c3bec125c7a94919ca2
msimangamina <149583783+msimangamina@users.noreply.github.com>
2023-11-27 12:04:28 +0200

    added more search engine functionality

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ function refreshWeather(response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeELement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000)
-
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document. querySelector("#icon");
+    
+    
+   
     cityElement.innerHTML = response.data.city;
-
     timeELement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
 }
 
