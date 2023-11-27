c3633e0663a230480eac43a307a00e5d719ea028
msimangamina <149583783+msimangamina@users.noreply.github.com>
2023-11-27 09:35:55 +0200

    Added search engine functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+    let apiKey = "64bcb800d50dtc6fc4a227d492o33327";
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(refreshWeather);
+      
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 
 }
 
@@ -10,3 +29,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
+
+searchCity("Paris");
